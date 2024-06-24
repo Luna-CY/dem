b0f99b769410cb8e64a99ae2719d1cd6cbb44c8b
萤火 <luna@cyl-mail.com>
2024-06-24 11:54:31 +0800

    修复shell命令的bug

--- internal/utils/cmd.go
+++ internal/utils/cmd.go
@@ -10,7 +10,12 @@ import (
 
 // ExecuteShellCommand 执行Shell命令
 func ExecuteShellCommand(ctx context.Context, command string, output io.Writer) error {
-	var cmd = exec.CommandContext(ctx, filepath.Base(os.Getenv("SHELL")), "-c", command)
+	var shell = filepath.Base(os.Getenv("SHELL"))
+	if "" == shell {
+		shell = "sh"
+	}
+
+	var cmd = exec.CommandContext(ctx, shell, "-c", command)
 
 	if nil != output {
 		cmd.Stdout = output
